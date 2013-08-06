0c41f244ba54befdff9252bf2be7828ac5061bde
Julian Stecklina <jsteckli@os.inf.tu-dresden.de>
2013-08-06 18:04:21 +0200

    A limit of 0 now means unlimited. Implemented active mode.

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ benchcat
 Like a simple version of netcat plus bandwidth throttling
 capabilities. Benchcat will write zero bytes to anyone who
 connects. Every single connection will be throttled to the maximum
-bandwidth.
+throughput.
 
 Lightly tested on Linux and FreeBSD.
--- benchcat.c
+++ benchcat.c
@@ -3,7 +3,7 @@
 /*
  * Benchcat
  *
- * Copyright (C) 2012, Julian Stecklina <jsteckli@os.inf.tu-dresden.de>
+ * Copyright (C) 2012-2013, Julian Stecklina <jsteckli@os.inf.tu-dresden.de>
  * Economic rights: Technische Universitaet Dresden (Germany)
  *
  * This file is part of benchcat.
@@ -35,6 +35,7 @@
 #include <pthread.h>
 #include <errno.h>
 #include <sys/errno.h>
+#include <arpa/inet.h>
 
 #ifdef __linux__
 # include <sys/sendfile.h>
@@ -46,15 +47,17 @@
 
 #define MAX_CHUNK (2*1024*1024)
 
-static uint64_t bytes_per_second;
-static uint16_t external_port;
-static int      devzero;
-static bool     receiving = true;
+static uint64_t    bytes_per_second;
+static const char *ip_addr;
+static uint16_t    external_port;
+static int         devzero;
+static bool        active    = false;
+static bool        receiving = true;
 
 static void
 print_help(void)
 {
-  fprintf(stderr, "Usage: limit-in-mbit port send/recv\n");
+  fprintf(stderr, "Usage: ip port limit-in-mbit active/passive send/recv\n");
 }
 
 static __thread struct timespec last_call;
@@ -62,6 +65,10 @@ static __thread struct timespec last_call;
 static uint32_t get_budget()
 {
   struct timespec now;
+
+  /* Interpret a limit of 0 as unlimited. */
+  if (bytes_per_second == 0)
+    return MAX_CHUNK;
 
  again:
   clock_gettime(CLOCK_MONOTONIC, &now);
@@ -118,6 +125,11 @@ static void *handler_fn(void *p)
   if (setsockopt(sock, SOL_SOCKET, receiving ? SO_RCVBUF : SO_SNDBUF, &sz, sizeof(sz)) < 0)
     goto close_it;
 
+
+  struct timespec start, end;
+
+  clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+
   if (receiving) {
     void *buf = malloc(MAX_CHUNK);
 
@@ -132,8 +144,10 @@ static void *handler_fn(void *p)
       bytes_sent += cur;
   }
 
+  clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+
  close_it:
-  printf("Sent %llu bytes.\n", bytes_sent);
+  printf("%llu bytes in total.\n", bytes_sent);
   perror("xmit");
   close(sock);
   return NULL;
@@ -145,7 +159,6 @@ int main(int argc, char **argv)
      a connection. */
   if (signal(SIGPIPE, SIG_IGN) == SIG_ERR) { perror("signal"); return EXIT_FAILURE; };
 
-
   /* Create temporary file */
   char tmpn[] = "/tmp/benchcat.XXXXXXXX";
   devzero = mkstemp(tmpn);
@@ -156,15 +169,23 @@ int main(int argc, char **argv)
   if (write(devzero, "\0", 1) != 1)
     { perror("write"); return EXIT_FAILURE; }
 
-  if (argc != 4) {
+  if (argc != 6) {
     print_help();
     return EXIT_FAILURE;
   }
   
-  bytes_per_second = strtoull(argv[1], NULL, 0) * 1000 * 1000 / 8;
+  ip_addr          = argv[1];
   external_port    = strtoul(argv[2], NULL, 0);
-  receiving        = (strcmp(argv[3], "recv") == 0);
-  if (!receiving && (strcmp(argv[3], "send") != 0)) {
+  bytes_per_second = strtoull(argv[3], NULL, 0) * 1000 * 1000 / 8;
+
+  active           = (strcmp(argv[4], "active") == 0);
+  if (!active && (strcmp(argv[4], "passive") != 0)) {
+    fprintf(stderr, "Parameter must be 'active' or 'passive'\n");
+    return EXIT_FAILURE;
+  }
+
+  receiving        = (strcmp(argv[5], "recv") == 0);
+  if (!receiving && (strcmp(argv[5], "send") != 0)) {
     fprintf(stderr, "Last parameter must be 'send' or 'recv'\n");
     return EXIT_FAILURE;
   }
@@ -187,25 +208,38 @@ int main(int argc, char **argv)
 
   my_addr.sin_family = AF_INET;
   my_addr.sin_port   = htons(external_port);
-  my_addr.sin_addr.s_addr = htonl (INADDR_ANY);
-  
-  if (bind(sock, (struct sockaddr *)&my_addr, sizeof(my_addr)) < 0) {
-    perror("bind"); return EXIT_FAILURE;
+  if (inet_pton(AF_INET, ip_addr, &my_addr.sin_addr.s_addr) == 0) {
+    perror("inet_pton"); return EXIT_FAILURE;
   }
 
-  if (listen(sock, 10) < 0) { perror("listen"); return EXIT_FAILURE; }
-
-  int psock;
-  socklen_t addrlen = sizeof(peer_addr);
-  while ((psock = accept(sock, (struct sockaddr *)&peer_addr, &addrlen)) >= 0) {
-    printf("Accepted connection.\n");
-    addrlen = sizeof(peer_addr);
-    pthread_t p;
-    if (pthread_create(&p, NULL, handler_fn, (void *)(uintptr_t)psock) != 0) {
-      perror("pthread_create"); return EXIT_FAILURE;
+  if (active) {
+    /* Active */
+    if (connect(sock, &my_addr, sizeof(my_addr)) != 0) {
+      perror("connect");
+      return EXIT_FAILURE;
     }
-  }
+    handler_fn((void *)(uintptr_t)sock);
+  } else {
+    /* Passive */
+  
+    if (bind(sock, (struct sockaddr *)&my_addr, sizeof(my_addr)) < 0) {
+      perror("bind"); return EXIT_FAILURE;
+    }
 
-  perror("accept");
-  return EXIT_FAILURE;
+    if (listen(sock, 10) < 0) { perror("listen"); return EXIT_FAILURE; }
+
+    int psock;
+    socklen_t addrlen = sizeof(peer_addr);
+    while ((psock = accept(sock, (struct sockaddr *)&peer_addr, &addrlen)) >= 0) {
+      printf("Accepted connection.\n");
+      addrlen = sizeof(peer_addr);
+      pthread_t p;
+      if (pthread_create(&p, NULL, handler_fn, (void *)(uintptr_t)psock) != 0) {
+	perror("pthread_create"); return EXIT_FAILURE;
+      }
+    }
+
+    perror("accept");
+    return EXIT_FAILURE;
+  }
 }
